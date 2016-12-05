fc9ec977fd7dd27cd369a4c26e32f38ee1362f88
mayrandy <andreas.mayr@fau.de>
2016-12-05 17:15:03 +0100

    init

---
+++ test/sim.R
@@ -0,0 +1,8 @@
+
+set.seed(123)
+dat <- simJM(n = 400, n_i = 3, alpha = .5, beta = c(1,2,3), betals = c(2,3,1), betatimeind = 3,
+            lambda = 0.6)
+
+JMboost(y =dat$y, data = dat$X, Xls = dat$Xls, last = dat$last,
+           delta = dat$delta, id = dat$id, time = dat$time, lambda = 1, alpha = 0.1,
+           10, m_stop_ls = 10, step.length = .1, betatimeind = 3)
